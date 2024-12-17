2c4182b42d3dc2c6042c8a2d6efc94746014c74e
FFE <fferri@mercadona.es>
2024-12-17 13:56:02 +0100

    updated

--- Formula/fran.rb
+++ Formula/fran.rb
@@ -1,10 +1,10 @@
 class Fran < Formula
   desc "a cli for simple tasks in your localhost, franferri.io apis wrapper"
-  homepage "https://github.com/franferri/cli"
+  homepage "https://github.com/franferri/homebrew-cli"
   
   on_macos do
     if Hardware::CPU.arm? # macOS ARM64 (Apple Silicon)
-      url "https://github.com/franferri/cli/releases/download/v1.0.0/fran-macos-arm64"
+      url "https://github.com/franferri/homebrew-cli/releases/download/v1.0.0/fran-macos-arm64"
       sha256 "xxxxxxxx"
     elsif Hardware::CPU.intel?
       odie "This formula is only supported on macOS ARM64 (Apple Silicon)."
